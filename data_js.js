d915a6c26a378b53926d06065c3ec4c4184899fd
sandhya gautam <82304756+sandhyagautamm@users.noreply.github.com>
2022-07-01 19:34:04 +0545

    file uploaded

---
+++ data_js.js
@@ -0,0 +1,61 @@
+var popup=document.getElementsByClassName(".pop_up");
+var displaylog =document.getElementsByClassName(".login");
+var displayreg =document.getElementsByClassName(".reg_whole");
+var reg_head = document.getElementsByClassName(".regestration_head");
+function display_login(){
+    reg_head.style.backgroundColor="rgb(171, 32, 60)";
+      displayreg.style.display="none";
+     displaylog.style.display="block";
+}
+function display_reg(){
+
+    displayreg.style.display="block";
+   displaylog.style.display="none";
+}
+var usernameInput = document.querySelector(".inputtext");
+var passwordInput = document.querySelector(".inputps");
+var formText = document.querySelectorAll(".text");
+
+usernameInput.addEventListener("blur",()=>{
+    if(usernameInput.value){
+            formText[0].style.top = -5+"px";
+            formText[0].style.color="rgb(19, 19, 103)";
+    }
+
+});
+passwordInput.addEventListener("blur",()=>{
+    if(passwordInput.value){
+            formText[1].style.top = -5+"px";
+            formText[0].style.color="rgb(19, 19, 103)";
+    }
+});
+// registration part
+function validate(){
+    var name_pattern=/^[a-zA-Z]{5,30}$/;
+    var phoneno_pattern=/^98+[0-9]{8}$/;
+    var email_patter=/^w+\@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    var password_pattern=/^W+$/;
+    var name=document.getElementsById(".name").value;
+    var phoneno=document.getElementsById(".phoneno").value;
+    var email=document.getElementsById(".email").value;
+    var password=document.getElementsById(".password").value;
+    var confirmpw=document.getElementsById(".confirmpw").value;
+    if(name.search(name_pattern)<0){
+        alert("name pattern didn't match.");
+        document.getElementsByClassName(".one").innerHTML="*name pattern didnt match";
+        return false;
+    }
+    if(email.search(email_pattern)<0){
+        alert("email pattern didn't match");
+        document.getElementsByClassName(".three").innerHTML="*email pattern didnt match";
+        return false;
+    }
+    if(phoneno.search(phoneno_pattern)<0){
+        document.getElementsByClassName(".two").innerHTML="* phone number should be 10 digits";
+        return false;
+    }
+    if(password!=confirmpw){
+        document.getElementsByClassName(".five").innerHTML="* two password didn't match"; 
+       return false;
+    }   
+}
